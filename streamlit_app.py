aa545f6c725162ddde1a871095d6403818ccdd8d
lucaslins21 <lucas.lemos61@gmail.com>
2025-05-09 12:55:15 -0300

    fix: update page icon to use public GitHub URL for favicon

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.set_page_config(
     page_title="iFoodStats",
-    page_icon="https://github.com/lucaslins21/ifood-dashboard-app/blob/main/img/favicon.png",
+    page_icon="https://github.com/lucaslins21/ifood-dashboard-app/blob/main/img/favicon.png?raw=true",
     layout="wide"
 )
 
